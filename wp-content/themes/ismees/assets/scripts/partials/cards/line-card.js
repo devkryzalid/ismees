5e8ef1553fb607a667c21dccc22dca6b58271e5b
olelouer-kryzalid <olelouer@kryzalid.net>
2023-05-29 18:03:49 -0400

    reorganise front page code

--- wp-content/themes/ismees/assets/scripts/partials/cards/line-card.js
+++ wp-content/themes/ismees/assets/scripts/partials/cards/line-card.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const lineCard = document.querySelectorAll(".line-card");
+
+    lineCard.forEach((card) => {
+        card.addEventListener('mouseover', (event) => {
+            const cardRect = card.getBoundingClientRect();
+            const xPercentage = (event.clientX - cardRect.right) / cardRect.width;
+            const image = card.querySelector('.image-ctn');
+
+            image.style.top = event.offsetY + "px";
+
+            const minRight = 35;
+            const maxRight = 70;
+            
+            const right = minRight + (maxRight - minRight) * xPercentage;
+            image.style.right = `${right}px`;
+        });
+    });
 });
