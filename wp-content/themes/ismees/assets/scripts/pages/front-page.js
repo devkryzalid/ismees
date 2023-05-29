68085f7be1c1741589de67c3cb827f6dd285ae5c
olelouer-kryzalid <olelouer@kryzalid.net>
2023-05-29 12:42:26 -0400

    home page stylized

--- wp-content/themes/ismees/assets/scripts/pages/front-page.js
+++ wp-content/themes/ismees/assets/scripts/pages/front-page.js
@@ -2,6 +2,7 @@ import '../partials/horizontal-slider'
 
 document.addEventListener("DOMContentLoaded", () => {
     const columns = document.querySelectorAll('.ctas-column');
+    const baseUrl = window.location.origin;
 
     // INCREASE THE WIDTH OF THE IMAGE AND THE COLUMN OF THE HOVERED COLUMN
     // REDUCE THE WIDTH OF THE OTHER
@@ -43,4 +44,89 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     window.dispatchEvent(new Event("resize"));
+
+
+    // SET THE HOMEPAGE IMAGES ANIMATION
+
+    // Image sources. You can add more overlays and images if you need.
+    const images = [
+        {
+            banner: 'http://ismees.local/wp-content/themes/ismees/assets/images/home-images/slide-01.png',
+            overlays: [
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-ligne-1.svg`,
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-asterisque.svg`
+            ]
+        },
+        {
+            banner: 'http://ismees.local/wp-content/themes/ismees/assets/images/home-images/slide-02.png',
+            overlays: [
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-etoile.svg`,
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-ligne-2.svg`,
+            ]
+        },
+        {
+            banner: 'http://ismees.local/wp-content/themes/ismees/assets/images/home-images/slide-03.png',
+            overlays: [
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-ligne-3.svg`,
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-asterisque.svg`,
+            ]
+        },
+        {
+            banner: 'http://ismees.local/wp-content/themes/ismees/assets/images/home-images/slide-04.png',
+            overlays: [
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-ligne-5.svg`,
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-asterisque.svg`
+            ]
+          },
+          {
+            banner: 'http://ismees.local/wp-content/themes/ismees/assets/images/home-images/slide-05.png',
+            overlays: [
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-ligne-4.svg`,
+                `${baseUrl}/wp-content/themes/ismees/assets/images/home-images/handdraw-asterisque.svg`
+            ]
+          },
+    ];
+
+    //Set the background color of the before element for each image => it must be set in the SCSS file with a new class
+    const colorClasses = ['-yellow', '-mint', '-blue', '-salmon','-purple'];
+  
+    let currentImageIndex = 0;
+    const bannerImage = document.getElementById('bannerImageCtn');
+
+    function changeImage() {
+        // Remove the old image
+        while (bannerImage.firstChild) {
+            bannerImage.firstChild.remove();
+        }
+
+        // Remove old color classes and add new one
+        colorClasses.forEach((colorClass) => {
+            bannerImage.classList.remove(colorClass);
+        });
+        bannerImage.classList.add(colorClasses[currentImageIndex % colorClasses.length]);
+
+        // Create a new image
+        const newImage = document.createElement('img');
+        newImage.src = images[currentImageIndex].banner;
+        newImage.classList.add('banner-img');
+        bannerImage.appendChild(newImage);
+
+        // Create overlay images
+        images[currentImageIndex].overlays.forEach((overlayImage, index) => {
+            const newOverlayImage = document.createElement('img');
+            newOverlayImage.src = overlayImage;
+            newOverlayImage.classList.add(`overlay-image`);
+            newOverlayImage.classList.add(`overlay-image__${currentImageIndex}-${index}`);
+            bannerImage.appendChild(newOverlayImage);
+        });
+
+        // Prepare the index for the next image
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+
+    // Call the function immediately to change the image when the page loads
+    changeImage();
+
+    // Adjust the interval here
+    setInterval(changeImage, 4000);
 });
