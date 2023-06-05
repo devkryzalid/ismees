4ff481718d13ad3dee26d7ff843a8b9564ac5937
olelouer-kryzalid <olelouer@kryzalid.net>
2023-06-05 15:05:23 -0400

    addsearch function

--- wp-content/themes/ismees/dist/scripts/pages/intervention.js
+++ wp-content/themes/ismees/dist/scripts/pages/intervention.js
@@ -1,1 +1,108 @@
-!function(){var t={5891:function(){document.addEventListener("DOMContentLoaded",(()=>{document.querySelectorAll(".line-card").forEach((t=>{t.addEventListener("mouseover",(e=>{const n=t.getBoundingClientRect(),r=(e.clientX-n.right)/n.width,o=t.querySelector(".image-ctn");o.style.top=e.offsetY+"px";const u=35+35*r;o.style.right=`${u}px`}))}))}))}},e={};function n(r){var o=e[r];if(void 0!==o)return o.exports;var u=e[r]={exports:{}};return t[r](u,u.exports,n),u.exports}n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,{a:e}),e},n.d=function(t,e){for(var r in e)n.o(e,r)&&!n.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:e[r]})},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},function(){"use strict";n(5891)}()}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/scripts/pages/intervention.js":
+/*!**********************************************!*\
+  !*** ./assets/scripts/pages/intervention.js ***!
+  \**********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _partials_cards_line_card__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../partials/cards/line-card */ \"./assets/scripts/partials/cards/line-card.js\");\n/* harmony import */ var _partials_cards_line_card__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_partials_cards_line_card__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack://ismees/./assets/scripts/pages/intervention.js?");
+
+/***/ }),
+
+/***/ "./assets/scripts/partials/cards/line-card.js":
+/*!****************************************************!*\
+  !*** ./assets/scripts/partials/cards/line-card.js ***!
+  \****************************************************/
+/***/ (function() {
+
+eval("document.addEventListener(\"DOMContentLoaded\", () => {\n    const lineCard = document.querySelectorAll(\".line-card\");\n\n    lineCard.forEach((card) => {\n        card.addEventListener('mouseover', (event) => {\n            const cardRect = card.getBoundingClientRect();\n            const xPercentage = (event.clientX - cardRect.right) / cardRect.width;\n            const image = card.querySelector('.image-ctn');\n\n            image.style.top = event.offsetY + \"px\";\n\n            const minRight = 35;\n            const maxRight = 70;\n            \n            const right = minRight + (maxRight - minRight) * xPercentage;\n            image.style.right = `${right}px`;\n        });\n    });\n});\n\n//# sourceURL=webpack://ismees/./assets/scripts/partials/cards/line-card.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./assets/scripts/pages/intervention.js");
+/******/ 	
+/******/ })()
+;
--- wp-content/themes/ismees/inc/search.php
+++ wp-content/themes/ismees/inc/search.php
@@ -11,3 +11,36 @@ function search_filter( $query ) {
 		}
 	}
 }
+/**
+ * searchByAddsearch
+ *
+ * @param  string 	$search_term	Required because Addsearch return 400 without this param
+ * @param  array 	$simple_params 	Array for page, limit and lang filter
+ * @param  array 	$custom_fields 	Array for custom filed saving in Addsearch
+ * @return object 	$response		Object response for addsearch api curl
+ */
+function searchByAddsearch(string $search_term, array $simple_params = ['limit' => 10, 'page' => 1, 'lang' => 'fr'], array $custom_fields = null)
+{
+    $url = 'https://api.addsearch.com/v1/search/' . ADDSEARCH_SITE_KEY;
+    $term = $params = $cf_query = '';
+    $separator = '?';
+
+    if ($search_term) {
+        $term .= $separator . 'term=' . str_replace(' ', '+', stripslashes($search_term));
+        $separator = '&';
+    }
+    foreach ($simple_params as $p_name => $p_value) {
+        $params .= $separator . $p_name . '=' . urlencode(stripslashes($p_value));
+    }
+    if (!empty($custom_fields)) {
+        foreach ($custom_fields as $field_name => $cf_values) {
+            foreach ($cf_values as $cf_value) {
+                $cf_query .= $separator . 'customField=' . $field_name . '%3D' . urlencode(stripslashes($cf_value));
+            }
+        }
+    }
+
+    $response = Requests::get($url . $term . $params . $cf_query);
+    return $response;
+
+}
