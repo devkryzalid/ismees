4760cea1038d96e665c84c384e0c72da260f9797
olelouer-kryzalid <olelouer@kryzalid.net>
2023-06-06 19:34:55 -0400

    remove unecessaries elements from the addsearch research

--- wp-content/themes/ismees/templates/research.php
+++ wp-content/themes/ismees/templates/research.php
@@ -1,9 +1,10 @@
 <?php
 /*
- *	Template Name: Recherche
+ * Template Name: Recherche
  */
 require_once 'wp-content/themes/ismees/inc/search.php';
 
+// Setting up the context and parameters for the search
 $context      = Timber::context();
 $category     = empty($_GET['category']) ? null : $_GET['category'];
 $type         = empty($_GET['type']) ? null : $_GET['type'];
@@ -12,31 +13,34 @@ $paged        = empty($_GET['pagenb']) ? 1 : $_GET['pagenb'];
 $search       = empty($_GET['search']) ? ' ' : $_GET['search'];
 $member       = empty($_GET['member']) ? null : $_GET['member'];
 
+// Initialize an empty hits array
+$hits = [];
 
+// Checking if a search term is provided
 if (!empty($search)) {
     $custom_fields = [];
 
+    // Add category to custom fields if it's not empty
     if (!empty($category)) {
         $custom_fields['category'] = [$category];
     }
 
+    // Checking if member flag is set and if it's true
     if (!empty($member) && $member == true) {
         if (!empty($type)) {
             $custom_fields['resource_member_type'] = [$type];
         }
 
-        // $context['subjects'] = new Timber\PostQuery($member_thematics);
         $context['types'] = get_terms(['taxonomy' => 'resource_member_type']);
-
     } else {
         if (!empty($type)) {
             $custom_fields['resource_student_type'] = [$type];
         }
 
         $context['types'] = get_terms(['taxonomy' => 'resource_student_type']);
-        // $context['subjects'] = new Timber\PostQuery($student_subjects);
     }
 
+    // Check if subjects parameter is present
     if (!empty($subjects)) {
         if (!is_array($subjects)) {
             $subjects = explode(',', $subjects);
@@ -44,24 +48,31 @@ if (!empty($search)) {
         $custom_fields['subjects'] = $subjects;
     }
 
+    // Call the searchByAddsearch function to fetch the response
     $response = searchByAddsearch($search, ['limit' => 15, 'page' => $paged], $custom_fields);
 
+    // Decode the JSON response body
     $results = json_decode($response->body);
 
-    // dd($results);
+    // Iterate through each hit in the response
+    foreach($results->hits as $hit) {
+        // Check if custom field exists in the hit
 
-    // dd($results->total_hits);
+        if(isset($hit->custom_fields)) {
 
-    $hits = $results->hits;
-    // $total_items = $results->total_hits;
+            // Add hit to the hits array
+            $hits[] = $hit;
+        }
+    }
 }
 
+// Set the necessary parameters for the context
 $context['categories'] = get_terms(['taxonomy' => 'resource_category']);
 $context['member'] = $member;
 $context['results'] = $hits;
-// $context['total'] = new Timber\PostQuery($total_items);
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 $context['params'] = $_GET;
 
+// Render the research page with the given context
 Timber::render( array( 'pages/research.twig' ), $context );
