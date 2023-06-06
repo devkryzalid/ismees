3286e9eee6332656f2e3eaf94c6e19893768450d
olelouer-kryzalid <olelouer@kryzalid.net>
2023-06-06 19:18:01 -0400

    add new custom fields for addsearch

--- wp-content/themes/ismees/singles/single-student-resource.php
+++ wp-content/themes/ismees/singles/single-student-resource.php
@@ -8,15 +8,20 @@ $subjects = $timber_post->meta('subjects');
 
 //GET THE CUSTOMS FIELDS FOR ADDSEARCH
 $categories = wp_get_post_terms($timber_post->ID, 'resource_category', ['fields' => 'names']);
-$types = wp_get_post_terms($timber_post->ID, 'resource_student_type', ['fields' => 'names']);
+$type = get_primary_taxonomy($timber_post->ID, 'resource_student_type', true);
 $subjects_addSearch = $timber_post->meta('subjects');
+
+if ($type) {
+    $icon = get_field('icon', $type);
+}
 
 /**
  * Timber context assignments
  */
-$context['types'] = $types;
+$context['type'] = $type->name;
 $context['categories'] = $categories;
 $context['subjects_addSearch'] = $subjects_addSearch;
+$context['icon'] = $icon;
 
 $context['post'] = $timber_post;
 $context['related_posts'] = new Timber\PostQuery($subjects);
