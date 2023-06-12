a040c23f4031ff77367c804b4eb867cbce606abd
olelouer-kryzalid <olelouer@kryzalid.net>
2023-06-12 08:58:07 -0400

    remove dd

--- wp-content/themes/ismees/templates/hub.php
+++ wp-content/themes/ismees/templates/hub.php
@@ -12,9 +12,15 @@ $subjects = [
     'nopaging'       => true,
 ];
 
+$all_subjects = new Timber\PostQuery($subjects);
 
-$context['subjects'] = new Timber\PostQuery($subjects);
-dd($context['subjects']);
+// Filter out the subject with the slug 'aider-quelquun'
+$filtered_subjects = array_filter($all_subjects->get_posts(), function($subject) {
+    return $subject->post_name !== 'aider-quelquun';
+});
+
+$context['subjects'] = $filtered_subjects;
+// dd($context['subjects']);
 
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
