db666489ac3785ae75a1da96cd55664549aabd80
olelouer-kryzalid <olelouer@kryzalid.net>
2023-06-22 10:09:05 -0400

    MAJ WP

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2';
+$wp_version = '6.2.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -46,4 +46,4 @@ $required_php_version = '5.6.20';
  */
 $required_mysql_version = '5.0';
 
-$wp_local_package = 'fr_CA';
+$wp_local_package = 'fr_FR';
