b4882270c1cbe560f8d9f10a3050d3cafb231d33
olelouer-kryzalid <olelouer@kryzalid.net>
2023-06-28 17:03:00 -0400

    QA

--- wp-content/themes/ismees/assets/scripts/pages/single-thematic.js
+++ wp-content/themes/ismees/assets/scripts/pages/single-thematic.js
@@ -1,1 +1,10 @@
 import '../partials/header-reverse';
+
+// Change the header button color
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('ernio');
+    let interventionColor = document.getElementById('interventionColor').innerText;
+  
+    document.documentElement.style.setProperty('--button-color', interventionColor);
+  });
+  
